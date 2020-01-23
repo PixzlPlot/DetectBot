4776e6741f0439f5be60efea31b72767b0afa345
PixzlPlot <59046855+PixzlPlot@users.noreply.github.com>
2020-01-23 08:24:13 +0100

    Update DetectBot.py

--- DetectBot.py
+++ DetectBot.py
@@ -60,32 +60,28 @@
 
 
 
+
+
+#import required packages
+
 import sys
-
 import os
-
 import termios
-
 import tty
-
 import RPi.GPIO as GPIO
-
 import time
-
 import jetson.inference
-
 import jetson.utils
-
 import argparse
 
 
+#turn GPIO warnings off, some thimes when running multiple times one can get som warning notifications, ignore.
 GPIO.setwarnings(False)
 
 
+# set motor pint using board markings any GIPO will do i use the ones set in the demo.
 
-# set motor pint any GIPO will do
-
-# for right Motor on ENA
+# for right Motor
 ENA = 37
 IN1 = 35
 IN2 = 33
@@ -101,43 +97,30 @@ IN4 = 24
 GPIO.setmode(GPIO.BOARD)
 
  
-
 # initialize EnA, In1, In2, In3, In4
 
 GPIO.setup(ENA, GPIO.OUT, initial=GPIO.LOW)
-
 GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
-
 GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
-
 GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
-
 GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
 
  
 
- #Manual controll whit keybord
+ #Manual controll whit keybord, get's a char from keybord imput
 def getch():
-
     fd = sys.stdin.fileno()
-
     old_settings = termios.tcgetattr(fd)
-
     try:
-
         tty.setraw(sys.stdin.fileno())
-
         ch = sys.stdin.read(1)
-
     finally:
-
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-
     return ch
 
 
-
-# parse the command line
+#IMPORTANT changing width and height will make you in need to changing object detection prtameters on rows 350 and onwards.
+# parse the command line - part of the object detection program. Used to set optional parameters like width and height of camera pichure
 
 parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.",
 
@@ -147,19 +130,19 @@ parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre
 
 parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
 
-parser.add_argument("--threshold", type=float, default=0.0, help="minimum detection threshold to use")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use")
 
 parser.add_argument("--camera", type=str, default="0", help="index of the MIPI CSI camera to use (e.g. CSI camera 0)\nor for VL42 cameras, the /dev/video device to use.\nby default, MIPI CSI camera 0 will be used.")
 
-parser.add_argument("--width", type=int, default=1920, help="desired width of camera stream (default is 1280 pixels)")
+parser.add_argument("--width", type=int, default=1280, help="desired width of camera stream (default is 1280 pixels)")
 
-parser.add_argument("--height", type=int, default=1080, help="desired height of camera stream (default is 720 pixels)")
+parser.add_argument("--height", type=int, default=720, help="desired height of camera stream (default is 720 pixels)")
 
 
 
  
 
-#Time of manual control to be active
+#Time of manual control to be active, the program is usind delays so to small numbers will make the bot seam to hack when driving, to large will make it go to far befour taking a new detection pichure. 
 
 button_delay = 0.8 #How long the motors are driven befour repeating code
 auto_turn_delay =  10 #How long the turns on autopilot to be befour taking a new pichure (it is a divider of button delay)
@@ -169,13 +152,9 @@ auto_turn_delay =  10 #How long the turns on autopilot to be befour taking a new
  
 
 try:
-
     opt = parser.parse_known_args()[0]
-
 except:
-
     parser.print_help()
-
     sys.exit(0)
 
  
@@ -183,7 +162,6 @@ except:
 # load the object detection network
 net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 
- 
 # create the camera and display
 
 camera = jetson.utils.gstCamera(opt.width, opt.height, opt.camera)
@@ -198,70 +176,43 @@ while display.IsOpen():
 
  
 
-
 #Looks for keybord imput
 
     char = getch()
 
 
+#uncomment to use autopilot function "only goes forward"
     #Autopilot
     #print("Looking for item")	
-
   #  GPIO.output(IN1, GPIO.HIGH)
-
   #  GPIO.output(IN2, GPIO.HIGH)
-
   #  GPIO.output(IN3, GPIO.HIGH)
-
  #   GPIO.output(IN4, GPIO.HIGH)
 
 
-#If imput is found then:
 
- 
 
+#If imput is found then from get char:
+
+#char "p" stops the code execution: turns motors off and turns camera off, cleans upp GPIO.
     if (char == "p"):
 
-        print("Stop!")
+      print("Stop!")
+      GPIO.output(ENA, GPIO.LOW)
+      GPIO.output(IN1, GPIO.LOW)
+      GPIO.output(IN2, GPIO.LOW)
+      GPIO.output(IN3, GPIO.LOW)
+      GPIO.output(IN4, GPIO.LOW)
+      GPIO.cleanup()
+      exit(0)
 
-        GPIO.output(ENA, GPIO.LOW)
 
-        GPIO.output(IN1, GPIO.LOW)
-
-        GPIO.output(IN2, GPIO.LOW)
-
-        GPIO.output(IN3, GPIO.LOW)
-
-        GPIO.output(IN4, GPIO.LOW)
-
-	GPIO.cleanup()
-
-        exit(0)
-
+#Goes left and then turns motors off as a safety measure, can be removed but be careful
     if (char == "a"):
 
-        print("Left pressed")
-
-        GPIO.output(IN1, GPIO.LOW)
-
-        GPIO.output(IN2, GPIO.HIGH)
-
-        time.sleep(button_delay)
-        GPIO.output(ENA, GPIO.HIGH)
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.LOW)
-        GPIO.output(IN4, GPIO.LOW)
-
-    elif (char == "d"):
-
-      print("Right pressed")
-
-
-      GPIO.output(IN3, GPIO.HIGH)
-
-      GPIO.output(IN4, GPIO.LOW)
-
+      print("Left pressed")
+      GPIO.output(IN1, GPIO.LOW)
+      GPIO.output(IN2, GPIO.HIGH)
       time.sleep(button_delay)
       GPIO.output(ENA, GPIO.HIGH)
       GPIO.output(IN1, GPIO.LOW)
@@ -269,39 +220,56 @@ while display.IsOpen():
       GPIO.output(IN3, GPIO.LOW)
       GPIO.output(IN4, GPIO.LOW)
 
+
+
+#Goes right and then turns motors off as a safety measure, can be removed but be careful
+    elif (char == "d"):
+
+      print("Right pressed")
+      GPIO.output(IN3, GPIO.HIGH)
+      GPIO.output(IN4, GPIO.LOW)
+      time.sleep(button_delay)
+      GPIO.output(ENA, GPIO.HIGH)
+      GPIO.output(IN1, GPIO.LOW)
+      GPIO.output(IN2, GPIO.LOW)
+      GPIO.output(IN3, GPIO.LOW)
+      GPIO.output(IN4, GPIO.LOW)
+
+
+
+#Goes forward and then turns motors off as a safety measure, can be removed but be careful
     elif (char == "w"):
 
-        print("Up pressed")
-        GPIO.output(IN3, GPIO.HIGH)
-        GPIO.output(IN4, GPIO.LOW)
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.HIGH)
-        time.sleep(button_delay)
-        GPIO.output(ENA, GPIO.HIGH)
-        GPIO.output(IN1, GPIO.LOW)
-	GPIO.output(IN2, GPIO.LOW)
-	GPIO.output(IN3, GPIO.LOW)
-	GPIO.output(IN4, GPIO.LOW)
+      print("Up pressed")
+      GPIO.output(IN3, GPIO.HIGH)
+      GPIO.output(IN4, GPIO.LOW)
+      GPIO.output(IN1, GPIO.LOW)
+      GPIO.output(IN2, GPIO.HIGH)
+      time.sleep(button_delay)
+      GPIO.output(ENA, GPIO.HIGH)
+      GPIO.output(IN1, GPIO.LOW)
+      GPIO.output(IN2, GPIO.LOW)
+    	GPIO.output(IN3, GPIO.LOW)
+      GPIO.output(IN4, GPIO.LOW)
 
  
+ 
+ 
+ 
+#Goes bakwards and then turns motors off as a safety measure, can be removed but be careful
     elif (char == "s"):
 
-        print("Down pressed")
-
-        GPIO.output(IN3, GPIO.LOW)
-
-        GPIO.output(IN4, GPIO.HIGH)
-
-        GPIO.output(IN1, GPIO.HIGH)
-
-        GPIO.output(IN2, GPIO.LOW)
-
-        time.sleep(button_delay)
-        GPIO.output(ENA, GPIO.HIGH)
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.LOW)
-        GPIO.output(IN4, GPIO.LOW)
+      print("Down pressed")
+      GPIO.output(IN3, GPIO.LOW)
+      GPIO.output(IN4, GPIO.HIGH)
+      GPIO.output(IN1, GPIO.HIGH)
+      GPIO.output(IN2, GPIO.LOW)
+      time.sleep(button_delay)
+      GPIO.output(ENA, GPIO.HIGH)
+      GPIO.output(IN1, GPIO.LOW)
+      GPIO.output(IN2, GPIO.LOW)
+      GPIO.output(IN3, GPIO.LOW)
+      GPIO.output(IN4, GPIO.LOW)
 
  
  
@@ -310,7 +278,6 @@ while display.IsOpen():
 
     img, width, height = camera.CaptureRGBA()
 
- 
 
 # detect objects in the image (with overlay)
 
@@ -322,77 +289,63 @@ while display.IsOpen():
         DetectedCenter = detection.Center[0]
         ClassID = detection.ClassID
 
+
+ #IMPORTANT this setings handel the object detection set the number of the object you want to detect below ClassID can be set for multiple objects i've set for CandyNet change for other networks
+ 
  #If there is a obstcle
     # backup and go left
         if (ClassID == 100): 
 
-            print("Going around")
-
-            GPIO.output(IN1, GPIO.LOW)
-
-            GPIO.output(IN2, GPIO.LOW)
-
-            GPIO.output(IN3, GPIO.LOW)
-
-      	    GPIO.output(IN4, GPIO.LOW)
-
-            time.sleep(button_delay / auto_turn_delay)
-
-            GPIO.output(IN1, GPIO.HIGH)
-
-            GPIO.output(IN2, GPIO.HIGH)
-
-            GPIO.output(IN3, GPIO.HIGH)
-
-            GPIO.output(IN4, GPIO.HIGH)
-
-            time.sleep(button_delay)
+          print("Going around")
+          GPIO.output(IN1, GPIO.LOW)
+          GPIO.output(IN2, GPIO.LOW)
+          GPIO.output(IN3, GPIO.LOW)
+    	    GPIO.output(IN4, GPIO.LOW)
+          time.sleep(button_delay / auto_turn_delay)
+          GPIO.output(IN1, GPIO.HIGH)
+          GPIO.output(IN2, GPIO.HIGH)
+          GPIO.output(IN3, GPIO.HIGH)
+          GPIO.output(IN4, GPIO.HIGH)
+          time.sleep(button_delay)
 
 
  
 
-#If object is detected then go left if bottle is to the left of the center
+#If object is detected then go left if objcet is to the left of the center
 # and go right if the bottle is to the right of the center
 
         elif (ClassID == 0): #INPUT NUMBER-CODE OF OBJECT TO BE FOLLOWED
-	    print("Candy!")	
+     print("Candy!")	
 
 	#Detecting object on the right side of the screen
             if(DetectedCenter < 640): # go little to the left and then forward
 
-	        print("Going left")
-
-      		GPIO.output(IN1, GPIO.LOW)
-
-      		GPIO.output(IN2, GPIO.HIGH)
-
-                time.sleep(button_delay / auto_turn_delay)
-	        GPIO.output(ENA, GPIO.HIGH)
-       	        GPIO.output(IN1, GPIO.LOW)
-       	        GPIO.output(IN2, GPIO.LOW)
-       	        GPIO.output(IN3, GPIO.LOW)
-       	        GPIO.output(IN4, GPIO.LOW)
-		break
-
+              print("Going left") 
+              GPIO.output(IN1, GPIO.LOW)
+              GPIO.output(IN2, GPIO.HIGH)
+              time.sleep(button_delay / auto_turn_delay)
+              GPIO.output(ENA, GPIO.HIGH)
+       	      GPIO.output(IN1, GPIO.LOW)
+       	      GPIO.output(IN2, GPIO.LOW)
+       	      GPIO.output(IN3, GPIO.LOW)
+       	      GPIO.output(IN4, GPIO.LOW)
+              break
 
 
  
 	#Detecting object on the right side of the screen
             elif (DetectedCenter > 640): # go little to the right and then forward
-	    
-	        print("Going right")
-
-      		GPIO.output(IN3, GPIO.HIGH)
-
-      		GPIO.output(IN4, GPIO.LOW)
-
-                time.sleep(button_delay*4/ auto_turn_delay)
-	        GPIO.output(ENA, GPIO.HIGH)
-       	        GPIO.output(IN1, GPIO.LOW)
-       	        GPIO.output(IN2, GPIO.LOW)
-       	        GPIO.output(IN3, GPIO.LOW)
-       	        GPIO.output(IN4, GPIO.LOW)
-		break
+              print("Going right")
+              GPIO.output(IN3, GPIO.HIGH)
+              GPIO.output(IN4, GPIO.LOW)
+              GPIO.output(IN4, GPIO.LOW)
+              time.sleep(button_delay*4/ auto_turn_delay)
+              GPIO.output(ENA, GPIO.HIGH)
+       	      GPIO.output(IN1, GPIO.LOW)
+       	      GPIO.output(IN2, GPIO.LOW)
+       	      GPIO.output(IN3, GPIO.LOW)
+       	      GPIO.output(IN4, GPIO.LOW)
+              break
 
  
 
